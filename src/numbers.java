603d27afb2c3e6075f1c4cd13154dbf2ca59052e
liccix <149318081+liccix@users.noreply.github.com>
2023-10-29 19:25:23 +0300

    Update numbers.java

--- src/numbers.java
+++ src/numbers.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class numbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите строку: ");
+        String inputString = scanner.nextLine();
+
+        System.out.print("Введите натуральное число N: ");
+        int n = scanner.nextInt();
+
+        if (n <= 0) {
+            System.out.println(inputString);
+        } else {
+            for (int i = 0; i < n; i++) {
+                System.out.println(inputString);
+            }
+        }
+    }
 }
