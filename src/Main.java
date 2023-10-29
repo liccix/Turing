289c7c0fdfedf881129efc7ba6e6fb016a64ccca
liccix <149318081+liccix@users.noreply.github.com>
2023-10-29 19:26:16 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,28 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Введите строку: ");
-        String inputString = scanner.nextLine();
+        System.out.print("Введите количество раз в неделю: ");
+        int timesPerWeek = scanner.nextInt();
 
-        System.out.print("Введите натуральное число N: ");
-        int n = scanner.nextInt();
+        System.out.print("Введите количество литров жидкости за каждый раз: ");
+        int litersPerTime = scanner.nextInt();
 
-        // Если n меньше или равно 0, выводим строку хотя бы один раз
-        if (n <= 0) {
-            System.out.println(inputString);
+        if (timesPerWeek <= 0 || litersPerTime <= 0) {
+            System.out.println("Введены некорректные данные");
         } else {
-            for (int i = 0; i < n; i++) {
-                System.out.println(inputString);
+            int totalLiters = timesPerWeek * litersPerTime * 16;
+
+            System.out.println("Суммарное количество литров за каждую неделю:");
+
+            for (int week = 1; week <= 16; week++) {
+                int weeklyTotal = timesPerWeek * litersPerTime * week;
+                System.out.println("К " + week + " неделе будет выпито  " + weeklyTotal + " литров жидкости");
+            }
+
+            if (totalLiters >= 120) {
+                System.out.println("Студент принят в IT-клуб!");
+            } else {
+                System.out.println("К сожалению, студент не принят в клуб");
             }
         }
     }
